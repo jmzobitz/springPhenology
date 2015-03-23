6bac4b2ed97d3b6ad6aa81711bcb6dca40923e77
jmzobitz <zobitz@augsburg.edu>
2015-03-23 14:57:44 -0500

    3/23/16: Revision of the files that start to analyze the temperature data that will be downloaded.

--- helperFunctions/temperatureAnalyze.R
+++ helperFunctions/temperatureAnalyze.R
@@ -1,11 +1,18 @@
-### Function that runs an R script to flag and makr the temperature data
+### Author: JMZ
+### Modified: 3/26/15
+### Purpose: Initial start of functions that take downloaded MODIS LST data and organizes them by year and day for analysis.
+### The input will be a list of MODIS pixels that we will then analyze
+
+### Known issues: Need to make sure flag files are downloaded, and this file will be added.
+
+### Function that runs an R script to flag and make the temperature data
 
 # Source the 
 library(MODISTools)
 
 
 # Link to LST product: https://lpdaac.usgs.gov/products/modis_products_table/mod11a2
-5000
+
 #test
 ### Identify the files needed to load up the data
 dirSave = "MODISData"
@@ -13,71 +20,30 @@ qcBand = "QC_day"
 bandName = "LST_Day_1km"
 scaleFactor = .02;
 
+
 # Call up the temperature and the data flags
  temperatureData=MODISTimeSeries(Dir = dirSave, Band = bandName,Simplify=TRUE)
- temperatureFlag=MODISTimeSeries(Dir = dirSave, Band = qcBand,Simplify=TRUE)
-
+# temperatureFlag=MODISTimeSeries(Dir = dirSave, Band = qcBand,Simplify=TRUE)
 # Mark the temperature flags that are invalid data as NA (see link above)
+# temperatureData[which(temperatureFlag!=0)]=NA;
+# - 3/25/15: temperature flags are not incorporated yet.
 
-# temp[which(tempFlag!=0)]=NA;
-
-# Multiply by the scale factor and then 
-# temp = temp*scaleFactor - 273.15;
-# 
-# # remove row names
-# rownames(temp)=NULL
-# 
-# # Convert days to decimal days for ease of access
-# dataTimes = strptime(row.names(temp),format="A%Y%j");
-# year = as.numeric(format(dataTimes, "%Y"))
-# day = as.numeric(format(dataTimes, "%j"))
-# 
-# # Make a data frame from results
-# temperatureData = data.frame(yearValue = year, dayValue=day,temp)
-# 
-# 
-# 
-# data=MODISTimeSeries(Dir = dirSave, Band = bands[2],Simplify=TRUE)
-# dataFlag=MODISTimeSeries(Dir = dirSave, Band = bands[1],Simplify=TRUE)
-# 
-# # Remove the data the are not of good quality (see the MODIS product )
-# data[which(dataFlag!=0)]=NA;
-# 
-# 
-# 
-# 
+# Define a quick function that converts the tempeature data from Kelvin to celsius
+convertKelvins <- function(x,scaleFactor) x*scaleFactor-273.15
 
 
-############################################
-## If all bands use the same QA band, they can be summarised together
-# MODISSummaries(LoadDat=dat, Dir = "MOD13Q1_IndividualPixel", Product = products, Bands = bands[2], ValidRange = c(-2000, 10000), NoDataFill = -3000, ScaleFactor = 0.0001, StartDate = TRUE, Interpolate = TRUE, Yield = TRUE, QualityScreen = TRUE, QualityBand = bands[1], QualityThreshold = 0, DiagnosticPlot = TRUE)
-# 
-# 
-# MODISSummaries(LoadDat=dat, Dir = "MOD13Q1_IndividualPixel", Product = products, Bands = bands[2], ValidRange = c(-2000, 10000), NoDataFill = -3000, ScaleFactor = 0.0001, StartDate = TRUE)
-# 
-## Should now be a file with the MODIS data appended to our original data in the folder "MOD13Q1_IndividualPixel", ready for analysis
+# Convert all of the temperature data to celsius
+temperatureData =lapply(temperatureData,convertCel,scaleFactor)
 
-# ############
-# ########## Larger Tiles ############
-# 
-# if(file.exists("MOD13Q1_6x6km") == FALSE)
-#   dir.create("MOD13Q1_6x6km")
-# 	
-# ## Call to download the subsets
-# MODISSubsets(dat, Products = products, Bands = bands, Size = c(3,3), SaveDir = "MOD13Q1_6x6km", TimeSeriesLength = 2)
-# 
-# 
-# # To make sure that all time-series were downloaded, run the data frame through update subsets
-# # to create a new data frame of all the site that might have failed to download first time through
-# take2 <- UpdateSubsets(dat,StartDate = FALSE, Dir = "MOD13Q1_6x6km")
-# 
-# # Now to use MODISSubsets again
-# MODISSubsets(take2, Products = products, Bands = bands, Size = c(3,3), SaveDir = "MOD13Q1_6x6km", TimeSeriesLength = 2)
-# 
-# MODISTimeSeries(Dir = "MOD13Q1_6x6km", Band = "250m_16_days_EVI")
-# 
-# ############################################
-# ## If all bands use the same QA band, they can be summarised together
-# MODISSummaries(dat, Dir = "MOD13Q1_6x6km", Product = products, Bands = bands[2], ValidRange = c(-2000, 10000), NoDataFill = -3000, ScaleFactor = 0.0001, StartDate = FALSE, Interpolate = TRUE, Yield = TRUE, QualityScreen = TRUE, QualityBand = "250m_16_days_pixel_reliability", QualityThreshold = 1)
-# 
-# ## Should now be a file with the MODIS data appended to our original data in the folder "MOD13Q1_6x6km", ready for analysis
+# Define a quick function that returns the year, day, and temperature from a given list data
+dataFrameTemperature <- function(x) {
+  dataTimes = strptime(row.names(x),format="A%Y%j");
+  rownames(x)=NULL
+  year = as.numeric(format(dataTimes, "%Y"))
+  day = as.numeric(format(dataTimes, "%j"))
+  return(data.frame(year,day,temperature=x[,1]))
+  
+}
+
+
+
